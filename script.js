3b30948877c4d555938e4837d18d5c623474c978
wolfet262 <tyler.wolfe262@outlook.com>
2020-12-18 08:56:07 -0800

    added all three results to the page

--- script.js
+++ script.js
@@ -19,9 +19,11 @@ async function getData(event) {
     .then((res) => res.json())
     .then((res) => {
       const topResult = document.querySelector("#firstResult");
-      topResult.innerHTML = `We think your top nationality is: ${res.country[0].country_id} with a ${res.country[0].probability.toFixed(2)}% probability!`;
-      
+      topResult.innerHTML = `We think your top nationality is: ${res.country[0].country_id} with a ${res.country[0].probability.toFixed(2) * 100}% probability!`;
+      const nextResult = document.querySelector("#secondResult");
+      nextResult.innerHTML = `Our next guess would be: ${res.country[1].country_id} with a ${res.country[1].probability.toFixed(2) * 100}% probability.`;
+      const lastResult = document.querySelector("#thirdResult");
+      lastResult.innerHTML = `Our final guess is: ${res.country[2].country_id} with a ${res.country[2].probability.toFixed(2) * 100}% probability`;
     })
     .catch((err) => console.log("somthing went wrong...", err));
 }
-//var= probability = num * 100
